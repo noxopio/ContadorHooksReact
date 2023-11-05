3d116227787566384bf971c1698475619de975ed
nox <dark.tempus@gmail.com>
2023-11-04 20:41:24 -0500

    first commit

---
+++ HokksApp.jsx
@@ -0,0 +1,15 @@
+import { ContadorComponent } from "./components/ContadorComponent"
+export const HokksApp = () => {
+  return (
+   
+   
+   
+    <>
+    <h1>Aplicacion de Hooks</h1>
+    <hr />
+    <ContadorComponent></ContadorComponent>
+
+    
+    </>
+  )
+}
